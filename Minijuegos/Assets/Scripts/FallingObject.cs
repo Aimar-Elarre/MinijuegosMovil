285c7ca35d39c82a52ed569fad7d1b74146769a3
Markel16 <markel_nunez@euneiz.com>
2025-11-04 19:29:47 +0100

    add
    problema arreglado de commits,projectbakup

--- Minijuegos/Assets/Scripts/FallingObject.cs
+++ Minijuegos/Assets/Scripts/FallingObject.cs
@@ -1,17 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FallingObject : MonoBehaviour
 {
-    [SerializeField] private float speed = 4f;
+    [SerializeField] private float speed = 4f;  
+
+    
+    public void SetSpeed(float s)
+    {
+        speed = s;
+    }
 
     private void Update()
     {
         transform.Translate(Vector3.down * speed * Time.deltaTime);
 
-        // si sale muy abajo de la pantalla, destruir
-        if (transform.position.y < -6f)
+        if (Camera.main != null)
         {
-            Destroy(gameObject);
+            var bottom = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, -0.2f, Mathf.Abs(Camera.main.transform.position.z) + 1f));
+            if (transform.position.y < bottom.y)
+                Destroy(gameObject);
         }
     }
 }
