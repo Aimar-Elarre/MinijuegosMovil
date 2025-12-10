d2ac1badbc08a8a2cb82b8989dfaa284ae91ffa4
Markel16 <markel_nunez@euneiz.com>
2025-12-10 19:21:31 +0100

    fruit manager creado

---
+++ Minijuegos/Assets/Scripts/FruitManager.cs
@@ -0,0 +1,32 @@
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class FruitManager : MonoBehaviour
+{
+    public GameObject[] frutas;        // modelos disponibles
+    public Transform spawnPoint;
+    public Basket basket;
+    public int puntuacion = 0;
+
+    private GameObject frutaActual;
+
+    void SpawnFruta()
+    {
+        int r = Random.Range(0, frutas.Length);
+        frutaActual = Instantiate(frutas[r], spawnPoint.position, Quaternion.identity);
+    }
+
+    public void Acierto()
+    {
+        puntuacion++;
+        Destroy(frutaActual);
+        SpawnFruta();
+    }
+
+    public void Error()
+    {
+        puntuacion--;
+        Destroy(frutaActual);
+        SpawnFruta();
+    }
+}
