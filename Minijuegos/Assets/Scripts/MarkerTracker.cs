b459528d49d8571791c64978c2ec0c51b8f9ad9c
Aimar <aimar_elarre@euneiz.com>
2025-12-18 18:22:29 +0100

    fix
    arreglo final de ar

--- Minijuegos/Assets/Scripts/MarkerTracker.cs
+++ Minijuegos/Assets/Scripts/MarkerTracker.cs
@@ -1,9 +1,12 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 public class MarkerTracker : MonoBehaviour
 {
+    public GameObject cestaMoneda;
+    public GameObject cestaMonstruo;
+
     public ARTrackedImageManager trackedImageManager;
     public GameManagerAR gameManager;
 
@@ -21,21 +24,56 @@ public class MarkerTracker : MonoBehaviour
     {
         foreach (var img in args.added)
         {
-            if (img.trackingState == TrackingState.Tracking)
-                gameManager.OnMarkerFound();
+            UpdateImage(img);
         }
 
         foreach (var img in args.updated)
         {
-            if (img.trackingState == TrackingState.Tracking)
-                gameManager.OnMarkerFound();
-            else
-                gameManager.OnMarkerLost();
+            UpdateImage(img);
         }
 
         foreach (var img in args.removed)
         {
+            ClearImage(img);
             gameManager.OnMarkerLost();
+        }
+    }
+
+    void UpdateImage(ARTrackedImage img)
+    {
+        //SIEMPRE limpiamos primero (clave para evitar conflictos)
+        ClearImage(img);
+
+        if (img.trackingState != TrackingState.Tracking)
+        {
+            gameManager.OnMarkerLost();
+            return;
+        }
+
+        gameManager.OnMarkerFound();
+
+        GameObject prefab = null;
+
+        if (img.referenceImage.name == "imagen_monstruo")
+            prefab = cestaMonstruo;
+        else if (img.referenceImage.name == "imagen_moneda")
+            prefab = cestaMoneda;
+
+        if (prefab == null)
+            return;
+
+        GameObject obj = Instantiate(prefab);
+        obj.transform.SetParent(img.transform, false);
+        obj.transform.localPosition = Vector3.zero;
+        obj.transform.localRotation = Quaternion.identity;
+        obj.transform.localScale = Vector3.one;
+    }
+
+    void ClearImage(ARTrackedImage img)
+    {
+        for (int i = img.transform.childCount - 1; i >= 0; i--)
+        {
+            Destroy(img.transform.GetChild(i).gameObject);
         }
     }
 }
